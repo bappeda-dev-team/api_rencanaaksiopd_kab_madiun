eeeaafbc2feadc2642e3bd80b0af96ef95c11cd8
agnarbriantama <agnarbriantama25@student.uns.ac.id>
2025-05-17 18:48:42 +0700

    db

--- app/database.go
+++ app/database.go
@@ -23,15 +23,19 @@ func GetConnection() *sql.DB {
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST")
 
-	connStr := fmt.Sprintf("%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbHost, dbPort, dbName)
-
 	log.Printf("Mencoba koneksi ke database %s di %s:%s...", dbName, dbHost, dbPort)
 
-	// dbUrl := os.Getenv("DB_URL")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
 
-	// if dbUrl != "" {
-	// 	connStr = dbUrl
-	// }
+	connStr := fmt.Sprintf("%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbHost, dbPort, dbName)
+
+	dbUrl := os.Getenv("DB_URL")
+
+	if dbUrl != "" {
+		connStr = dbUrl
+	}
 
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
